ab1d6bea28eaa4083d89788d8cf257f9e21dbe7a
HRedal <hector.redal@ericsson.com>
2024-03-07 08:15:00 +0100

    Fixing some errors in the definition of the aesd-assignment and ldd package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,16 +7,17 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 305472f052e05b01aa1929d4a2c5d5e5911af8a8
+AESD_ASSIGNMENTS_VERSION = 428aa72cb5d2a102427ee78cb5a00dfac563cf88
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-4-HRedal.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-5-HRedal.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -28,5 +29,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	
+	$(INSTALL) -d 0766 $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 endef
 
+$(eval $(generic-package))
--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LDD_VERSION = v2.6.37
-LDD_SITE = https://github.com/cu-ecen-aeld/ldd3.git
+LDD_VERSION = 5cff85c900f7577e2c5e38abe5d6027eac5b0872
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-HRedal.git
 LDD_SITE_METHOD = git
 LDD_LICENSE = GPL-2.0
 LDD_LICENSE_FILES = COPYING
@@ -14,12 +14,11 @@ LDD_MODULE_SUBDIRS = scull
 LDD_MODULE_SUBDIRS += misc-modules
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define LDD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/scull/scull_load   $(TARGET_DIR)/sbin/
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/sbin/
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/bin/
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/bin/misc-modules/
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load   $(TARGET_DIR)/sbin/
+ 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/sbin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load   $(TARGET_DIR)/sbin/
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/sbin/
 endef
 
 $(eval $(kernel-module))
